c57070abe45d6dca0b64012bd82b5b8f2bf025a1
Roberto Carrillo <email@robertocarrillo.com>
2015-09-28 19:41:29 -0500

    Updated to .NET 4.0

--- NHibernate.Generics/GenericAccessor.cs
+++ NHibernate.Generics/GenericAccessor.cs
@@ -31,7 +31,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
-using NHibernate.Properties;
+using NHibernate.Property;
 
 namespace NHibernate.Generics
 {
